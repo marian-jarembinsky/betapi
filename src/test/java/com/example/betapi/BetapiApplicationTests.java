a381d63cc8d6b5bce320335c0539cd876ff1ba1f
marianjarembinsky <jarembinsky.marian@gmail.com>
2025-11-21 18:46:24 +0100

    Initial commit

---
+++ src/test/java/com/example/betapi/BetapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.betapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
